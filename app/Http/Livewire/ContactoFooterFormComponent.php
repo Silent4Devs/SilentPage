ab9e9082b0f1da5d3372021c90cf9e0defc6d60a
beni <benito.lopez.tecno@gmail.com>
2023-08-25 08:47:45 -0600

    correo

--- app/Http/Livewire/ContactoFooterFormComponent.php
+++ app/Http/Livewire/ContactoFooterFormComponent.php
@@ -16,12 +16,11 @@ class ContactoFooterFormComponent extends Component
         'email' => 'required|email|max:255',
     ];
 
-
-
     public function render()
     {
         return view('livewire.contacto-footer-form-component');
     }
+
     public function submitForm()
     {
         $validatedData = $this->validate();
@@ -43,33 +42,33 @@ class ContactoFooterFormComponent extends Component
 
             header('Content-type: text/html; charset=UTF-8');
                 $body = <<<HTML
-        <div style="width: 90%;
-                    max-width: 450px;
-                    padding: 60px 20px;
-                    box-sizing: border-box;
-                    border-left: 1px solid #f5f5f5;
-                    border-right: 1px solid #f5f5f5;
-                    border-top: 30px solid #2375A6;
-                    border-bottom: 30px solid #2375A6;
-                    font-family: 'Segoe UI Regular';
-                    color: #696969;
-                    background-color: #f5f5f5;
-                    margin: auto;">
-            <h1 style="width: 100%;
-                        margin: 0;
-                        margin-top: 0px;
-                        font-size: 20px;
-                        text-align: center;
-                        color: #2375A6;">
-                Silent4business <br> <small style="font-weight: lighter; font-size:17px; color: #696969;"> Newsletter </small>
-            </h1>
-            <p style="margin-top: 30px;
-                        font-size: 14px;
-                        text-align: center;">
-                Suscripción a <strong> Newsletter: </strong> <br><br> <strong style="font-size: 16px;">$correo</strong>
-            </p>
-        </div>
-    HTML;
+                    <div style="width: 90%;
+                                max-width: 450px;
+                                padding: 60px 20px;
+                                box-sizing: border-box;
+                                border-left: 1px solid #f5f5f5;
+                                border-right: 1px solid #f5f5f5;
+                                border-top: 30px solid #2375A6;
+                                border-bottom: 30px solid #2375A6;
+                                font-family: 'Segoe UI Regular';
+                                color: #696969;
+                                background-color: #f5f5f5;
+                                margin: auto;">
+                        <h1 style="width: 100%;
+                                    margin: 0;
+                                    margin-top: 0px;
+                                    font-size: 20px;
+                                    text-align: center;
+                                    color: #2375A6;">
+                            Silent4business <br> <small style="font-weight: lighter; font-size:17px; color: #696969;"> Newsletter </small>
+                        </h1>
+                        <p style="margin-top: 30px;
+                                    font-size: 14px;
+                                    text-align: center;">
+                            Suscripción a <strong> Newsletter: </strong> <br><br> <strong style="font-size: 16px;">$correo</strong>
+                        </p>
+                    </div>
+                HTML;
                 //Datos del correo remitente, destinatario, asunto.
                 $phpmailer->setFrom('s4b.website@silent4business.com', 'S4B Website');
                 $phpmailer->IsHTML(true);
