810c2a6c64ef9acd6e3fadd44395a4980730cf1e
beni <benito.lopez.tecno@gmail.com>
2023-09-06 08:20:19 -0600

    crg

---
+++ resources/views/emails/newsletter.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+    <meta charset="UTF-8">
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="supported-color-schemes" content="light dark">
+
+    <title>Document</title>
+
+    <style>
+        .content {
+            width: 500px;
+            max-width: 500px;
+            min-width: 500px;
+            background-color: #f3f3f3;
+            color: #444;
+        }
+
+        .tr-header td {
+            background-color: blue;
+            color: #fff;
+            padding: 13px;
+            vertical-align: middle;
+        }
+    </style>
+</head>
+
+<body>
+    <table class="content">
+        <tr class="tr-header">
+            <td>
+                <img src="" alt="">
+            </td>
+            <td>
+                Portal SILENT4BUSINESS
+            </td>
+        </tr>
+        <tr>
+            <td>
+                content
+            </td>
+        </tr>
+        <tr>
+            <td>
+                2023
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
--- resources/views/layouts/complementos/footer.blade.php
+++ resources/views/layouts/complementos/footer.blade.php
@@ -7,7 +7,8 @@
             <img src="{{ asset('img/logo_silent.webp') }}">
             <p>
                 <i class="fa-solid fa-location-dot"></i>
-                <a href="https://goo.gl/maps/DkTtghVWhaFoBKSLA?coh=178573&entry=tt" target="_blank"> Torre Murano Insurgentes Sur #2453 </a>
+                <a href="https://goo.gl/maps/DkTtghVWhaFoBKSLA?coh=178573&entry=tt" target="_blank"> Torre Murano
+                    Insurgentes Sur #2453 </a>
             </p>
             <p>
                 <i class="fa-solid fa-phone"></i>
@@ -15,7 +16,8 @@
             </p>
             <p>
                 <i class="fa-regular fa-envelope"></i>
-                <a href="mailto:marketing@silent4business.com?Subject=Portal%20S4B%20Contacto">marketing@silent4business.com</a>
+                <a
+                    href="mailto:marketing@silent4business.com?Subject=Portal%20S4B%20Contacto">marketing@silent4business.com</a>
             </p>
         </div>
         @livewire('contacto-footer-form-component')
@@ -28,9 +30,11 @@
 
             <div class="redes-icons" style="margin-top: 50px;">
                 <a href="https://www.instagram.com/team.s4b/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
-                <a href="https://www.linkedin.com/company/silent4business/mycompany/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="https://www.linkedin.com/company/silent4business/mycompany/" target="_blank"><i
+                        class="fa-brands fa-linkedin"></i></a>
                 <a href="https://twitter.com/silent4business" target="_blank"><i class="fa-brands fa-twitter"></i></a>
-                <a href="https://www.youtube.com/@silent4business944" target="_blank"><i class="fa-brands fa-youtube"></i></a>
+                <a href="https://www.youtube.com/@silent4business944" target="_blank"><i
+                        class="fa-brands fa-youtube"></i></a>
             </div>
         </div>
     </div>
@@ -42,40 +46,40 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 <script>
     // enviar correo de newsletter
-    $('#form-newsletter').submit(function(e) {
-        e.preventDefault();
-        $.ajax({
-            data: $('#form-newsletter').serialize(),
-            url: $(this).attr('action'),
-            type: $(this).attr('method'),
-            cache: false,
-            dataType: 'json',
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            },
+    // $('#form-newsletter').submit(function(e) {
+    //     e.preventDefault();
+    //     $.ajax({
+    //         data: $('#form-newsletter').serialize(),
+    //         url: $(this).attr('action'),
+    //         type: $(this).attr('method'),
+    //         cache: false,
+    //         dataType: 'json',
+    //         headers: {
+    //             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    //         },
 
-            beforeSend: function() {
-                console.log('pre');
-                $('footer button').addClass('pre');
-                $('footer button').attr('disabled', 'true');
-            },
-            success: function() {
-                console.log('post');
-                $('footer button').removeClass('pre');
-                $('footer button').addClass('post');
-                $('footer button i').removeClass('fa-circle-notch');
-                $('footer button i').removeClass('fa-spin');
-                $('footer button i').addClass('fa-check');
-                $('footer button').attr('disabled', 'true');
-                $('footer input').attr('disabled', 'true');
-            },
-            error: function() {
-                console.log('error');
-                $('footer button').addClass('error');
-                $('footer button').removeClass('pre');
-            }
-        });
-    });
+    //         beforeSend: function() {
+    //             console.log('pre');
+    //             $('footer button').addClass('pre');
+    //             $('footer button').attr('disabled', 'true');
+    //         },
+    //         success: function() {
+    //             console.log('post');
+    //             $('footer button').removeClass('pre');
+    //             $('footer button').addClass('post');
+    //             $('footer button i').removeClass('fa-circle-notch');
+    //             $('footer button i').removeClass('fa-spin');
+    //             $('footer button i').addClass('fa-check');
+    //             $('footer button').attr('disabled', 'true');
+    //             $('footer input').attr('disabled', 'true');
+    //         },
+    //         error: function() {
+    //             console.log('error');
+    //             $('footer button').addClass('error');
+    //             $('footer button').removeClass('pre');
+    //         }
+    //     });
+    // });
 
     // animaciones
     let semi_alto = $(window).height() - ($(window).height() / 2.6);
