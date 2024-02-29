01cfd39cca99185ccac538926dc98bb0a196261d
John Vo <vojohn95@gmail.com>
2024-02-29 15:55:49 -0600

    fx

--- app/Http/Middleware/SetXFrameOptions.php
+++ app/Http/Middleware/SetXFrameOptions.php
@@ -48,6 +48,7 @@ class SetXFrameOptions
         $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('X-Frame-Options', 'DENY');
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+        $response->header('Content-Security-Policy', "frame-ancestors 'none'");
 
         return $response;
     }
