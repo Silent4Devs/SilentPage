3213c3675e176fb088fd7599d4d88236f99dbe7c
vicvicto <vhraindiana@hotmail.com>
2024-12-03 13:17:11 -0600

    Traduccion de vista talento

--- resources/views/livewire/talento-form-component.blade.php
+++ resources/views/livewire/talento-form-component.blade.php
@@ -2,12 +2,12 @@
     <form wire:submit.prevent="submitForm" enctype="multipart/form-data">
         <div class="caja-flex form-item input-radio" style="justify-content: space-between;">
             <div>
-                <label for="estudiante"><strong>Soy estudiante</strong> </label>
+                <label for="estudiante"><strong>{{ __('form_talento.student') }}</strong> </label>
                 <input required type="radio" wire:model.lazy="tipo" name="tipo" id="estudiante" value="estudiante"
                     title="Soy estudiante">
             </div>
             <div>
-                <label for="profesional"><strong>Soy profesional</strong> </label>
+                <label for="profesional"><strong>{{ __('form_talento.professional') }}</strong> </label>
                 <input type="radio" wire:model.lazy="tipo" name="tipo" id="profesional" value="profesional"
                     title="Soy profesional">
             </div>
@@ -16,7 +16,7 @@
             <span class="error">{{ $message }}</span>
         @enderror
         <div class="form-item">
-            <label for="nombre">Nombre <sup>*</sup></label>
+            <label for="nombre">{{ __('form_talento.name') }}<sup>*</sup></label>
             <input required type="text" wire:model.lazy="nombre" name="nombre" id="nombre" maxlength="255"
                 title="Nombre">
         </div>
@@ -24,7 +24,7 @@
             <span class="error">{{ $message }}</span>
         @enderror
         <div class="form-item">
-            <label for="email">Correo electrónico <sup>*</sup></label>
+            <label for="email">{{ __('form_talento.email') }}<sup>*</sup></label>
             <input required type="email" wire:model.lazy="email" name="email" id="email" maxlength="255"
                 title="Correo electrónico">
         </div>
@@ -32,7 +32,7 @@
             <span class="error">{{ $message }}</span>
         @enderror
         <div class="form-item">
-            <label for="">Teléfono <sup>*</sup></label>
+            <label for="">{{ __('form_talento.telephone') }}<sup>*</sup></label>
             <input required type="tel" wire:model.lazy="telefono" pattern="[0-9]{10,15}" maxlength="15"
                 name="telefono" id="telefono" title="Teléfono">
         </div>
@@ -40,38 +40,38 @@
             <span class="error">{{ $message }}</span>
         @enderror
         <div class="form-item input-radio">
-            <label for="">¿Actualmente te encuentras laborando? <sup>*</sup></label>
+            <label for="">{{ __('form_talento.currently_working') }}<sup>*</sup></label>
             <div class="caja-flex" style="gap: 50px;">
                 <div>
-                    <label for="laboro_si">Si</label>
+                    <label for="laboro_si">{{ __('form_talento.yes') }}</label>
                     <input required type="radio" wire:model.lazy="laborando" name="laborando" id="laboro_si"
                         value="si" title="Si laboro">
                 </div>
                 <div>
-                    <label for="laboro_no">No</label>
+                    <label for="laboro_no">{{ __('form_talento.no') }}</label>
                     <input type="radio" wire:model.lazy="laborando" name="laborando" id="laboro_no" value="no"
                         title="No laboro">
                 </div>
             </div>
         </div>
         <div class="form-item input-cv">
-            <label for="cv">Carga tu CV en PDF<sup>*</sup></label>
-            <input required type="file" wire:model.lazy="cv" name="cv" id="cv"
-                accept="application/pdf" title="Carga tu CV en PDF" />
+            <label for="cv">{{ __('form_talento.CV') }}<sup>*</sup></label>
+            <input required type="file" wire:model.lazy="cv" name="cv" id="cv" accept="application/pdf"
+                title="Carga tu CV en PDF" />
         </div>
         <div class="form-item input-acepto">
             <input type="checkbox" required wire:model.lazy="terminos" name="terminos" id="terminos" value="1"
                 title="Acepto los términos de privacidad">
             <label for="terminos">
                 <a href="{{ route('terminos-privacidad') }}" target="_blanck">
-                    Acepto los términos de privacidad
+                    {{ __('form_talento.privacy') }}
                 </a>
             </label>
         </div>
         <div class="form-item caja-flex" style="justify-content: end; margin-top: 60px;">
             <button type="submit" wire:loading.class="pre">
                 <i class="fa-solid fa-circle-notch fa-spin"></i>
-                Enviar datos
+                {{ __('form_talento.send_data') }}
             </button>
         </div>
     </form>
