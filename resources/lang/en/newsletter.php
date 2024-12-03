a55e8812661acab4564de07036b32718ea6a23dd
vojohn95 <vojohn95@gmail.com>
2024-12-03 12:28:48 -0600

    footer

---
+++ resources/lang/en/newsletter.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'title' => 'Join our Newsletter',
+    'meta_desc' => 'Subscribe to stay informed about our services, products, and developments in the world of Cybersecurity',
+    'mail' => 'Email Address',
+    'enviar_correo' => 'Send',
+    'enviar' => 'Send',
+];
