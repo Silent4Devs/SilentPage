5c517f03460ca746a951919dea0dd8e11fd0ba4a
Eric Kevin Luna Martínez <eric.luna@becarios.silent4business.com>
2024-12-03 13:10:35 -0600

    Traduccion de soluciones

---
+++ resources/lang/en/soluciones.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'title' => 'Cybersecurity Solutions Tailored to You',
+    'meta_desc' => 'Discover how our cybersecurity solutions protect your organization by adapting to sector-specific needs.',
+    'body_class' => 'soluciones-body',
+    'header_title' => 'Solutions',
+    'header_desc' => 'Due to the growing incidence of data theft and other cyberattacks, companies are trying to be prepared to face and reduce the risks they are exposed to daily.',
+    'soluciones' => [
+        'salud' => [
+            'title' => 'Health',
+            'description' => 'Healthcare systems are redefining how medical care is delivered, increasing the use of virtual medicine and promoting partnerships among different technological players to provide comprehensive care to patients. Our service offering is designed to enable this type of digital transformation securely.'
+        ],
+        'financiera' => [
+            'title' => 'Financial',
+            'description' => 'We adapt to the constant evolution of customer demands for safer and more personalized financial services, both physical, virtual, and mobile.'
+        ],
+        'manofactura' => [
+            'title' => 'Manufacturing',
+            'description' => 'Manufacturing is in a constant transformation process. We take advantage of this new window of opportunities and challenges that test companies’ ability to face a digitally connected and insecure technological world.'
+        ],
+        'energia' => [
+            'title' => 'Energy and Resources',
+            'description' => 'In a constantly changing world, energy and resource companies face significant technological challenges. For this reason, we implement effective strategies across the value chain of oil and gas, electricity, water, and mining to protect their operations.'
+        ],
+        'agro' => [
+            'title' => 'Agro-industrial',
+            'description' => 'The use of new technologies to improve the performance, efficiency, and profitability of agro-industrial products is not exempt from cyber risks. Therefore, we offer specialized services in prevention, detection, and mitigation.'
+        ],
+        'academia' => [
+            'title' => 'Academia',
+            'description' => 'Educational organizations are redefining how teaching and management are carried out. Our service offering is designed to ensure that this type of digital transformation is implemented securely.'
+        ],
+    ]
+];
